5503fc4a1af7556289d1182560a6622ed43d0133
Diego Plaza <diego@plaza.ninja>
2019-04-11 18:16:08 -0400

    session store on database config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Fluxday::Application.config.session_store :cookie_store, key: '_tracker_session'
+Fluxday::Application.config.session_store :active_record_store, key: '_tracker_session'
